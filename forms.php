14c69b2b5f6ed884dc99573740228f8320e2d934
burdspencer <spencer.d.burd@wmich.edu>
2019-10-30 00:57:47 -0400

    v1.2
    Added php script to forms.php for file upload
    
    Current bug:
    Error messages display on forms.php before file is selected. Possible need for an If(file selected) statement?

--- forms.php
+++ forms.php
@@ -21,10 +21,36 @@
 		<br>
 		<br>
 		<div class="form">
-			<form action="upload.php" method="post" enctype="multipart/form-data">
+			<form action="#" method="post" enctype="multipart/form-data">
 		  Select file to upload:
 		  <input type="file" name="fileToUpload" id="fileToUpload">
 		  <input type="submit" value="Upload File" name="submit">
+
+			<?php
+				$target_dir = "uploads/";
+				$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+				echo "<br>";
+				echo "<br>";
+				echo "target file = $target_file"; //Debug
+				$uploadOk = 1; //Default state
+				$FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION)); // Check file type || Might remove this?
+				if (file_exists($target_file)) { //Check if file already exists
+				    echo "Sorry, file already exists.";
+				    $uploadOk = 0;
+				}
+				if ($_FILES["fileToUpload"]["size"] > 50000000) { //Limit the file size to 50KB to protect webserver
+				    echo "Sorry, your file is too large.";
+				    $uploadOk = 0;
+				}
+				if ($uploadOk == 0) { //If any errors occur, stop upload.
+    				echo "Sorry, your file was not uploaded.";}
+				else{
+					if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+						echo "The file". basename($_FILES["fileToUpload"]["name"])." has been uploaded.";}
+						echo "Sorry, there was an error uploading your file.";
+				}
+			?>
+
 		</div>
 	</center>
 </body>
