95818e06605667e88be5258bea99859a5e22ab82
burdspencer <spencer.d.burd@wmich.edu>
2019-10-30 04:38:53 -0400

    Forms.php now functional

--- forms.php
+++ forms.php
@@ -25,32 +25,35 @@
 		  Select file to upload:
 		  <input type="file" name="fileToUpload" id="fileToUpload">
 		  <input type="submit" value="Upload File" name="submit">
-
 			<?php
-				$target_dir = "uploads/";
-				$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-				echo "<br>";
-				echo "<br>";
-				echo "target file = $target_file"; //Debug
-				$uploadOk = 1; //Default state
-				$FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION)); // Check file type || Might remove this?
-				if (file_exists($target_file)) { //Check if file already exists
-				    echo "Sorry, file already exists.";
-				    $uploadOk = 0;
-				}
-				if ($_FILES["fileToUpload"]["size"] > 50000000) { //Limit the file size to 50KB to protect webserver
-				    echo "Sorry, your file is too large.";
-				    $uploadOk = 0;
-				}
-				if ($uploadOk == 0) { //If any errors occur, stop upload.
-    				echo "Sorry, your file was not uploaded.";}
-				else{
-					if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-						echo "The file". basename($_FILES["fileToUpload"]["name"])." has been uploaded.";}
-						echo "Sorry, there was an error uploading your file.";
+				if(isset($_POST['submit'])){
+				  $target_dir = "uploads/";
+				  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+				  $uploadOk = 1;
+				  $FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+				  // Check if file already exists
+				  if (file_exists($target_file)) {
+				      echo "Sorry, file already exists.";
+				      $uploadOk = 0;
+				  }
+				  // Check file size
+				  if ($_FILES["fileToUpload"]["size"] > 50000000) {
+				      echo "Sorry, your file is too large.";
+				      $uploadOk = 0;
+				  }
+				  // Check if $uploadOk is set to 0 by an error
+				  if ($uploadOk == 0) {
+				      echo "Sorry, your file was not uploaded.";}
+				  // if everything is ok, try to upload file
+				  else {
+				      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+				          echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";}
+				       else {
+				          echo "Sorry, there was an error uploading your file.";
+				      }
+				  }
 				}
 			?>
-
 		</div>
 	</center>
 </body>
